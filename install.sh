a7db7ad7189a4688e1d66a265adfbbcadaa9192c
0000xFFFF <coldmailgg@gmail.com>
2025-08-18 01:49:21 +0200

    new script

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 sudo ln -sfr aiid-describe /usr/local/bin/.
 sudo ln -sfr aiid-nsfw-detector /usr/local/bin/.
+sudo ln -sfr aiid-nsfw-detector-show /usr/local/bin/.
 sudo ln -sfr aiid-group /usr/local/bin/.
 
