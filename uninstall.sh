91d02e643b380ac90097036440c89068fe6cf9ac
0000xFFFF <coldmailgg@gmail.com>
2025-10-05 11:04:13 +0200

    fix uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -x
 sudo rm /usr/local/bin/aiid-describe
-sudo rm /usr/local/bin/aiid-nsfw-detector
-sudo rm /usr/local/bin/aiid-nsfw-detector-show
+sudo rm /usr/local/bin/aiid-show
 sudo rm /usr/local/bin/aiid-group
